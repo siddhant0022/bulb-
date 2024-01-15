7daa5faf484179557bddfea14d666fa78e7ac29e
siddhant0022 <148846164+siddhant0022@users.noreply.github.com>
2024-01-16 00:10:54 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,14 @@
+var bulb = document.querySelector("#bulb");
+var btn = document.querySelector("button");
+var flag= 0;
+
+btn.addEventListener("click",function(){
+  if(flag == 0){
+  bulb.style.backgroundColor = "yellow";
+  flag = 1;
+  }
+  else{
+    bulb.style.backgroundColor = "black";
+    flag = 0;
+  }
+})
